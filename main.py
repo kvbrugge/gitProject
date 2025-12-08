c9a743abccc5c9eb05853c19783268b6cf25e063
karolek06 <kvbrugge@gmail.com>
2025-12-08 14:18:48 +0100

    trzy

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 ddcfdd
+s2check
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
