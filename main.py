e82582a5381caf38779341407c2dedd092324245
karolek06 <kvbrugge@gmail.com>
2025-12-09 12:01:06 +0100

    kutas

--- main.py
+++ main.py
@@ -3,8 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-ddcfdd
-def print_hi(name):
+chuj ci w dupedef print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
